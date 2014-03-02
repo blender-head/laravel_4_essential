ee489ac845eaa6086997eed9c532e62deb167c65
Andre Hartiwan <blwfish705@gmail.com>
2014-03-02 17:06:08 +0700

    Finish Chapter 7 - Controllers

---
+++ app/controllers/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+	class ArticleController extends BaseController
+	{
+		public function showIndex()
+		{
+			return View::make('index');
+		}
+		
+		public function showSingle($articleId)
+		{
+			return View::make('single');
+		}
+
+		public function newArticle()
+		{
+			
+		}
+	}
